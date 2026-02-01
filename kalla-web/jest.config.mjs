c628eb30a44f99a68153d4e314703471a46b99ba
Zidni Mubarok <zidmubarock@gmail.com>
2026-02-01 22:18:47 +0700

    I don't know whether this part works or not.

---
+++ kalla-web/jest.config.mjs
@@ -0,0 +1,17 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.tsx', '**/__tests__/**/*.test.ts'],
+};
+
+export default createJestConfig(config);
